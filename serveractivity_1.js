ff4608e5f7a8a55381e5ae7fead0f6931d7c3421
unknown <user527@ALCHEMYCLOUD.CO.IN>
2023-07-11 07:26:44 +0530

    HI

--- serveractivity_1.js
+++ serveractivity_1.js
@@ -12,7 +12,6 @@ res.setHeader('Content-Type' , 'text/plain');
 let a= prompt("Please enter first number","");
 let b=  prompt("Please enter second number","");
 
-res.end("Hello world123\n);
 });
 
 
@@ -20,5 +19,5 @@ server.listen(port,hostname, () => {
 console.log(`Server running at http://${hostname}:${port}/`);
 console.log("Consoles HelloWorld");
 var c = parseInt(a) + parsrInt(b);
-console.log("The sum of " +a+ and "+b+" is: "+c);
+console.log(c);
 });
